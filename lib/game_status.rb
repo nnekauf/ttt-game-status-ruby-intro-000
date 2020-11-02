687d193e8d0f92d40881e41dc1ee27c642bbab56
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-11-02 01:10:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,9 +63,9 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board).include? ("X")
+  if WIN_COMBINATIONS.include? ("X")
     return "X"
-  elsif won?(board).include? ("O")
+  elsif WIN_COMBINATIONS.include? ("O")
     return "O"
   end
 end
